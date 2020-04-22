3cdebe85c7b7fbfb702e9e5c851b0477405ccf9f
fbzr <fabricio.spb@gmail.com>
2020-04-22 07:16:20 -0700

    create and run index queries

--- database/database.sql
+++ database/database.sql
@@ -25,3 +25,15 @@ CREATE TABLE recipe_ingredients (
     quantity VARCHAR(255),
     UNIQUE (recipe_id, ingredient_id)
 );
+
+ALTER TABLE recipes
+    ADD COLUMN image_url VARCHAR(255);
+
+-- Indexes
+CREATE INDEX idx_recipes_id ON recipes(id);
+CREATE INDEX idx_ingredients_id ON ingredients(id);
+CREATE INDEX idx_instructions_id ON instructions(id);
+CREATE INDEX idx_instructions_recipe_id ON instructions(recipe_id);
+CREATE INDEX idx_recipe_ingredients_id ON recipe_ingredients(id);
+CREATE INDEX idx_recipe_ingredients_recipe_id ON recipe_ingredients(recipe_id);
+CREATE INDEX idx_recipe_ingredients_ingredient_id ON recipe_ingredients(ingredient_id);
